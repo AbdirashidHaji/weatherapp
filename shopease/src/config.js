1735940c9cef2783e50b784ca63f24e1fef7c40d
Abdirashid Haji <abdirashid.ke@gmail.com>
2025-07-11 17:02:39 +0300

    src removed

---
+++ shopease/src/config.js
@@ -0,0 +1,5 @@
+// src/config.js
+export const WEATHER_API_CONFIG = {
+  API_KEY: 'e4824d8cde559e320a05d7252f5034e7', // Replace with your actual key
+  BASE_URL: 'https://api.openweathermap.org/data/2.5'
+};
